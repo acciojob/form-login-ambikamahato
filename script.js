0c05c47d3720e2c5cf03714dbe276005a1340518
Ambika Pd Mahato <ambikamahato53@gmail.com>
2023-06-14 10:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+    let fastname = document.getElementsByTagName("input")[0];
+	let lastname = document.getElementsByTagName("input")[1];
+	console.log(fastname.value, lastname.value); 
+	// alert(`${fastname.value} ${lastname.value}`);
+    
 
 }
+
+
